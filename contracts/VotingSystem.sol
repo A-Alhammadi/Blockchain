ce4724310830d99f0fb8714a0584c2f787236e55
A-Alhammadi <113705421+A-Alhammadi@users.noreply.github.com>
2023-10-26 02:54:13 -0500

    Add files via upload

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract VotingSystem {
+
+    event CandidateRegistered(uint256 candidateId, string name);
+
     // Struct for a single voter
     struct Voter {
         bool hasVoted;
@@ -21,13 +24,16 @@ contract VotingSystem {
     Candidate[] public candidates;
     
     // Register new candidate
-    function registerCandidate(string memory _name) public returns (uint) {
-        candidates.push(Candidate({
-            name: _name,
-            voteCount: 0
-        }));
-        return candidates.length - 1;
-    }
+   function registerCandidate(string memory _name) public returns (uint) {
+    candidates.push(Candidate({
+        name: _name,
+        voteCount: 0
+    }));
+    uint id = candidates.length - 1;
+    emit CandidateRegistered(id, _name);
+    return id;
+}
+
 
     // Register  voter (should have more controls to prevent double registration, etc)
     function registerVoter() public {
