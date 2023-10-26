aed480fd909c82c6903bf1c7558c3b2ecda7b206
A-Alhammadi <113705421+A-Alhammadi@users.noreply.github.com>
2023-10-26 04:16:14 -0500

    Add files via upload

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,176 @@
+let web3;
+let votingSystem;
+const contractAddress = "0x88549D7332DfdF5A504293FEb9a1B7682cd9c78A"; //build/contracts/json file, networks section
+const contractABI = [
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "candidateId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        }
+      ],
+      "name": "CandidateRegistered",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "candidates",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "voteCount",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "voters",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "hasVoted",
+          "type": "bool"
+        },
+        {
+          "internalType": "uint256",
+          "name": "votedCandidateId",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        }
+      ],
+      "name": "registerCandidate",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "registerVoter",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_candidateId",
+          "type": "uint256"
+        }
+      ],
+      "name": "vote",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getCandidatesCount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_candidateId",
+          "type": "uint256"
+        }
+      ],
+      "name": "getCandidateVoteCount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    }
+   ]; // ABI of your contract copied from same json file as address
 
+window.addEventListener('load', async () => {
+    if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+        await window.ethereum.enable(); // Request access to user's Ethereum wallet
+    } else {
+        console.error("No Ethereum provider found. Install MetaMask!");
+        return;
+    }
+
+    votingSystem = new web3.eth.Contract(contractABI, contractAddress);
+});
+
+async function registerCandidate() {
+    const candidateName = document.getElementById('candidateName').value;
+    const accounts = await web3.eth.getAccounts();
+    await votingSystem.methods.registerCandidate(candidateName).send({ from: accounts[0] });
+}
+
+async function registerVoter() {
+    const accounts = await web3.eth.getAccounts();
+    await votingSystem.methods.registerVoter().send({ from: accounts[0] });
+}
+
+async function voteForCandidate() {
+    const candidateId = document.getElementById('candidateId').value;
+    const accounts = await web3.eth.getAccounts();
+    await votingSystem.methods.vote(candidateId).send({ from: accounts[0] });
+}
