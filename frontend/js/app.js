5b8d7752b1df5b284fdd5e5a1929514b302c68b6
A-Alhammadi <113705421+A-Alhammadi@users.noreply.github.com>
2023-11-07 14:24:42 -0600

    Add files via upload

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,6 @@
 let web3;
 let votingSystem;
-const contractAddress = "0x34eea99Eb1aBC97c3ab553Ca0CD27a895f5c6299"; //build/contracts/json file, networks section
+const contractAddress = "0x85Da404Cdf9Ad4C6d2cBbeCcE0E72A76eE6a0162"; // Replace with your contract's address
 const contractABI = [
   {
     "anonymous": false,
@@ -205,141 +205,121 @@ const contractABI = [
     "stateMutability": "view",
     "type": "function"
   }
-]; // ABI of your contract copied from same json file as address
-
-
+]; // Replace with your contract's ABI
 
 window.addEventListener('load', async () => {
     if (window.ethereum) {
         web3 = new Web3(window.ethereum);
-        await window.ethereum.enable(); // Request access to user's Ethereum wallet
-        votingSystem = new web3.eth.Contract(contractABI, contractAddress);
-
-        // Subscribe to the CandidateRegistered event
-        votingSystem.events.CandidateRegistered()
-            .on('data', async (event) => {
-                console.log('Candidate registered:', event.returnValues);
-                await updateCandidateList(); // Call the function to update the list on the page
-            })
-            .on('error', console.error);
-
-        // Update candidate list on page load
-        await updateCandidateList();
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            votingSystem = new web3.eth.Contract(contractABI, contractAddress);
+            subscribeToEvents();
+            await updateCandidateList();
+        } catch (error) {
+            console.error("Access to Ethereum account was denied.", error);
+            // Update the UI to inform the user access was denied
+        }
     } else {
         console.error("No Ethereum provider found. Install MetaMask!");
-        return;
+        // Update the UI to inform the user to install MetaMask
     }
 });
 
-// ... rest of the functions like requestAccount, checkRegisteredVoter, etc...
+function subscribeToEvents() {
+    votingSystem.events.CandidateRegistered()
+        .on('data', async (event) => {
+            console.log('Candidate registered:', event.returnValues);
+            await updateCandidateList();
+        })
+        .on('error', console.error);
+}
 
 async function updateCandidateList() {
-    // ... function body remains the same...
-}
+    try {
+        const candidates = await votingSystem.methods.getAllCandidates().call();
+        const candidatesListElement = document.getElementById('candidates-list');
 
-async function requestAccount() {
-  await window.ethereum.request({ method: 'eth_requestAccounts' });
-}
+        if (!candidatesListElement) return; // Exit if the element is not on the current page
 
-async function checkRegisteredVoter() {
-  const addressToCheck = document.getElementById('addressToCheck').value;
-  const voterDetails = await votingSystem.methods.voters(addressToCheck).call();
-  const isVoter = voterDetails.hasVoted;
-  console.log(`Is registered voter: ${isVoter}`);
-  // You can update some DOM element with this info
+        candidatesListElement.innerHTML = '';
+
+        candidates.forEach((candidate, index) => {
+            const candidateElement = document.createElement('div');
+            candidateElement.textContent = `ID: ${index}, Name: ${candidate.name}, Votes: ${candidate.voteCount}`;
+            candidatesListElement.appendChild(candidateElement);
+        });
+    } catch (error) {
+        console.error("Error updating candidate list:", error);
+        // Update the UI to inform the user about the error
+    }
 }
 
 async function registerCandidate() {
   try {
-    const candidateName = document.getElementById('new-candidate-name').value;
+    const candidateNameInput = document.getElementById('new-candidate-name');
+    if (!candidateNameInput) {
+      console.error("Can't find the new candidate name input field.");
+      return;
+    }
+    const candidateName = candidateNameInput.value;
     const accounts = await web3.eth.getAccounts();
-      await votingSystem.methods.registerCandidate(candidateName).send({ from: accounts[0] });
-      console.log("Candidate registered successfully!");
-      // add inform the user about successful registration
+    await votingSystem.methods.registerCandidate(candidateName).send({ from: accounts[0] });
+    console.log("Candidate registered successfully!");
+    // add inform the user about successful registration
   } catch (error) {
-      console.error("Error registering candidate:", error);
-      // add inform the user about the error
+    console.error("Error registering candidate:", error);
+    // add inform the user about the error
   }
 }
 
-async function registerVoter() {
-  try {
-      const accounts = await web3.eth.getAccounts();
-      await votingSystem.methods.registerVoter().send({ from: accounts[0] });
-      console.log("Voter registered successfully!");
-      // add inform the user about successful registration
-  } catch (error) {
-      console.error("Error registering voter:", error);
-      // add inform the user about the error
-  }
-}
 
 async function voteForCandidate() {
-  try {
-      const candidateId = document.getElementById('candidateId').value;
-      const accounts = await web3.eth.getAccounts();
-      await votingSystem.methods.vote(candidateId).send({ from: accounts[0] });
-      console.log("Voted successfully!");
-      // add inform the user about successful voting
-  } catch (error) {
-      console.error("Error voting for candidate:", error);
-      // add inform the user about the error
-  }
+    try {
+        const candidateId = document.getElementById('candidateId').value;
+        if (isNaN(candidateId)) {
+            // Update the UI to prompt the user to input a valid ID
+            return;
+        }
+        const accounts = await web3.eth.getAccounts();
+        await votingSystem.methods.vote(candidateId).send({ from: accounts[0] });
+        console.log("Voted successfully!");
+        // Update the UI to inform the user about successful voting
+    } catch (error) {
+        console.error("Error voting for candidate:", error);
+        // Update the UI to inform the user about the error
+    }
 }
 
 async function startVoting() {
-  const accounts = await web3.eth.getAccounts();
-  await votingSystem.methods.startVoting().send({ from: accounts[0] });
+    try {
+        const accounts = await web3.eth.getAccounts();
+        await votingSystem.methods.startVoting().send({ from: accounts[0] });
+        console.log("Voting started successfully!");
+        // Update the UI to inform the user
+    } catch (error) {
+        console.error("Error starting the voting:", error);
+        // Update the UI to inform the user about the error
+    }
 }
 
 async function endVoting() {
-  const accounts = await web3.eth.getAccounts();
-  await votingSystem.methods.endVoting().send({ from: accounts[0] });
+    try {
+        const accounts = await web3.eth.getAccounts();
+        await votingSystem.methods.endVoting().send({ from: accounts[0] });
+        console.log("Voting ended successfully!");
+        // Update the UI to inform the user
+    } catch (error) {
+        console.error("Error ending the voting:", error);
+        // Update the UI to inform the user about the error
+    }
 }
-async function getCandidates() {
-  const candidates = await votingSystem.methods.getAllCandidates().call();
-  // Use the 'candidates' array to display on the voting page
-  console.log(candidates);
-  // Now you can update the DOM with these details to show a list of candidates
-}
-async function updateCandidateList() {
-  const candidates = await votingSystem.methods.getAllCandidates().call();
-  const candidatesListElement = document.getElementById('candidates-list');
 
-  // Clear existing candidate list
-  candidatesListElement.innerHTML = '';
+// Listen for account changes in MetaMask and reload the page to reset state
+window.ethereum.on('accountsChanged', (accounts) => {
+    window.location.reload();
+});
 
-  // Add new candidates to the list
-  candidates.forEach((candidate, index) => {
-    const candidateElement = document.createElement('div');
-    candidateElement.textContent = `ID: ${index}, Name: ${candidate.name}, Votes: ${candidate.voteCount}`;
-    candidatesListElement.appendChild(candidateElement);
-  });
-}
-votingSystem.events.CandidateRegistered()
-  .on('data', async (event) => {
-    console.log('Candidate registered:', event.returnValues);
-    await updateCandidateList(); // Call the function to update the list on the page
-  })
-  .on('error', console.error);
-
-/*async function displayCandidatesAndVotes() {
-  try {
-      const totalCandidates = await votingSystem.methods.getCandidatesCount().call();
-      let candidatesList = [];
-      
-      for (let i = 0; i < totalCandidates; i++) {
-          const candidate = await votingSystem.methods.candidates(i).call();
-          candidatesList.push({
-              name: candidate.name,
-              voteCount: candidate.voteCount
-          });
-      }
-      
-      // Update the UI with candidatesList
-      // Example: display in a table or list format on your results page
-  } catch (error) {
-      console.error("Error fetching candidates and their votes:", error);
-      // Optionally, inform the user about the error
-  }
-}*/
+// Listen for chain changes in MetaMask and reload the page to reset state
+window.ethereum.on('chainChanged', (chainId) => {
+    window.location.reload();
+});
